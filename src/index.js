7f07eded6a9f51b61a036626ecda73aae8951984
Nhung <hongnhung16052003@gmail.com>
2024-09-16 10:21:00 +0700

    feat: scanner qr code by camera

--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-    <ToastContainer
-      position="top-center"
-      autoClose={2500}
-      pauseOnHover
-      closeOnClick
-    />
-  </React.StrictMode>,
+  // <React.StrictMode>
+   <>
+      <App />
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        pauseOnHover
+        closeOnClick
+      />
+   </>
+  // </React.StrictMode>,
 )
 reportWebVitals()
